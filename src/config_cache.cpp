618d0a4c4181dbd6961da473a8d8c5384b9b4609
Celtic Minstrel <celtic.minstrel.ca@some.place>
2018-02-25 23:11:43 -0500

    Implement validation of the core game config WML Currently this is disabled by default Use --validate command-line argument to enable
    The following new features were added in the schema format:
    * Union, intersection, and list types
    * Keys can specify a union of known types on-the-fly
    * Key and tag names can include wildcards (* and ?, glob-style)
    * Tags support any_key=yes, which means unrecognized subtags will be ignored
    * Tags and keys support deprecated=yes (unimplemented)
    * Specify max=infinite instead of max=-1

--- src/config_cache.cpp
+++ src/config_cache.cpp
@@ -75,9 +75,9 @@ void config_cache::clear_defines()
 	defines_map_["WESNOTH_VERSION"] = preproc_define(game_config::wesnoth_version.str());
 }
 
-void config_cache::get_config(const std::string& file_path, config& cfg)
+void config_cache::get_config(const std::string& file_path, config& cfg, abstract_validator* validator)
 {
-	load_configs(file_path, cfg);
+	load_configs(file_path, cfg, validator);
 }
 
 void config_cache::write_file(std::string file_path, const config& cfg)
@@ -121,14 +121,14 @@ void config_cache::add_defines_map_diff(preproc_map& defines_map)
 	return config_cache_transaction::instance().add_defines_map_diff(defines_map);
 }
 
-void config_cache::read_configs(const std::string& file_path, config& cfg, preproc_map& defines_map)
+void config_cache::read_configs(const std::string& file_path, config& cfg, preproc_map& defines_map, abstract_validator* validator)
 {
 	//read the file and then write to the cache
 	filesystem::scoped_istream stream = preprocess_file(file_path, &defines_map);
-	read(cfg, *stream);
+	read(cfg, *stream, validator);
 }
 
-void config_cache::read_cache(const std::string& file_path, config& cfg)
+void config_cache::read_cache(const std::string& file_path, config& cfg, abstract_validator* validator)
 {
 	static const std::string extension = ".gz";
 
@@ -216,7 +216,7 @@ void config_cache::read_cache(const std::string& file_path, config& cfg)
 
 		preproc_map copy_map(make_copy_map());
 
-		read_configs(file_path, cfg, copy_map);
+		read_configs(file_path, cfg, copy_map, validator);
 		add_defines_map_diff(copy_map);
 
 		try {
@@ -237,7 +237,7 @@ void config_cache::read_cache(const std::string& file_path, config& cfg)
 	LOG_CACHE << "Loading plain config instead of cache\n";
 
 	preproc_map copy_map(make_copy_map());
-	read_configs(file_path, cfg, copy_map);
+	read_configs(file_path, cfg, copy_map, validator);
 	add_defines_map_diff(copy_map);
 }
 
@@ -265,16 +265,16 @@ void config_cache::read_defines_queue()
 	}
 }
 
-void config_cache::load_configs(const std::string& config_path, config& cfg)
+void config_cache::load_configs(const std::string& config_path, config& cfg, abstract_validator* validator)
 {
 	// Make sure that we have fake transaction if no real one is going on
 	fake_transaction fake;
 
 	if (use_cache_) {
-		read_cache(config_path, cfg);
+		read_cache(config_path, cfg, validator);
 	} else {
 		preproc_map copy_map(make_copy_map());
-		read_configs(config_path, cfg, copy_map);
+		read_configs(config_path, cfg, copy_map, validator);
 		add_defines_map_diff(copy_map);
 	}
 }
