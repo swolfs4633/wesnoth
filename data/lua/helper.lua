8ab2185472558420c2446267e61fcec053121dae
Celtic Minstrel <celtic.minstrel.ca@some.place>
2018-03-11 12:55:30 -0400

    Lua API reorganization: interface module deprecations

--- data/lua/helper.lua
+++ data/lua/helper.lua
@@ -57,12 +57,6 @@ function helper.modify_unit(filter, vars)
 	wesnoth.set_variable("LUA_modify_unit")
 end
 
---! Fakes the move of a unit satisfying the given @a filter to position @a x, @a y.
---! @note Usable only during WML actions.
-function helper.move_unit_fake(filter, to_x, to_y)
-	wesnoth.intf.move_unit_fake(filter, to_x, to_y)
-end
-
 -- Metatable that redirects access to wml.variable.proxy
 local proxy_var_mt = {
 	__metatable = "WML variables",
@@ -308,5 +302,6 @@ helper.shallow_literal = wesnoth.deprecate_api('helper.shallow_literal', 'wml.sh
 helper.shallow_parsed = wesnoth.deprecate_api('helper.shallow_parsed', 'wml.shallow_parsed', 1, nil, wml.shallow_parsed)
 helper.set_wml_var_metatable = wesnoth.deprecate_api('helper.set_wml_var_metatable', 'wml.variable.proxy', 1, nil, helper.set_wml_var_metatable)
 helper.set_wml_tag_metatable = wesnoth.deprecate_api('helper.set_wml_tag_metatable', 'wml.tag', 1, nil, helper.set_wml_tag_metatable)
+helper.move_unit_fake = wesnoth.deprecate_api('helper.move_unit_fake', 'wesnoth.intf.move_unit_fake', 1, nil, wesnoth.intf.move_unit_fake)
 
 return helper
